4e824ef7294dfc61592f3bc33b268748a18256dc
So.So <ggg@ggg.com>
2023-12-20 15:44:07 +0330

    fff

--- app/src/main/java/ir/test/app_test_git/MainActivity.java
+++ app/src/main/java/ir/test/app_test_git/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dfsdfsdfsdfsd
-        //dfsdfsdfsdfsd
+
         Toast.makeText(this, "test04", Toast.LENGTH_SHORT).show();
 //        Toast.makeText(this, "test045444444444555", Toast.LENGTH_SHORT).show();
     }
