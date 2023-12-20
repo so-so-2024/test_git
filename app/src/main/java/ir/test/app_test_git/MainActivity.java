dc9e7d24a0116ef74b754e0218c96a6fdb6efecc
So.So <ggg@ggg.com>
2023-12-20 14:41:57 +0330

    first

---
+++ app/src/main/java/ir/test/app_test_git/MainActivity.java
@@ -0,0 +1,15 @@
+package ir.test.app_test_git;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //dfsdfsdfsdfsd
+    }
+}
