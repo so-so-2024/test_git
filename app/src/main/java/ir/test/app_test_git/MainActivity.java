5f81d1ba583b9da8aa96c8d63a1f247a644ceedb
So.So <ggg@ggg.com>
2023-12-20 14:43:22 +0330

    first

--- app/src/main/java/ir/test/app_test_git/MainActivity.java
+++ app/src/main/java/ir/test/app_test_git/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //dfsdfsdfsdfsd
         //dfsdfsdfsdfsd
+        //dfsdfsdfsdfsd22222222222222
     }
 }
